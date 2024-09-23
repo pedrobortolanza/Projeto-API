adc59b248fe2bb53234dcd90d74f53b6645e08bd
Joaquinlima26 <limajoca25@gmaile.com>
2024-09-23 16:08:13 -0300

    MapGet

--- Controllers/ParticipanteController.cs
+++ Controllers/ParticipanteController.cs
@@ -4,6 +4,13 @@ public static class ParticipanteController
 {
     public static void AddRotasParticipante(this WebApplication app)
     {
+        var RotasParticipantes = app.MapGroup("/participantes");
+
+        RotasParticipantes.MapGet("/", async(AppDbContext db) =>
+        {
+            return await db.Participantes.ToListAsync();
+        });
+
         
     }
     
