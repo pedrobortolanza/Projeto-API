e5bc8c15d3d9aac41cf4a107f8582a443430004f
Joaquinlima26 <limajoca25@gmaile.com>
2024-09-23 16:15:40 -0300

    MapGet-id e MapPost

--- Controllers/ParticipanteController.cs
+++ Controllers/ParticipanteController.cs
@@ -11,6 +11,18 @@ public static class ParticipanteController
             return await db.Participantes.ToListAsync();
         });
 
+        RotasParticipantes.MapGet("/{id}", async (int id, AppDbContext db) =>
+        {
+            return await db.Participantes.FindAsync(id) is Participante participante ? Results.Ok(participante) : Results.NotFound();
+        });
+
+        RotasParticipantes.MapPost("/", async (Participante participante, AppDbContext db) =>
+        {
+            db.Participantes.Add(participante);
+            await db.SaveChangesAsync();
+
+            return Results.Created($"/{participante.Id}", participante);
+        });
         
     }
     
